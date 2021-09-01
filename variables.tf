31517372a6e1f117b31a81fcf0d7d547581beb9f
mdnurakmal <mdnurakmal@gmail.com>
2021-09-02 01:36:43 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,6 +14,6 @@ variable "region" {
 
 resource "null_resource" "enforce_remediation" {
   provisioner "local-exec" {
-    command = "echo ${data.google_project.id}"
+    command = "echo ${data.google_project.project}"
   }
 }
