5006df2639ba69fc94adb6aba805cb1a5209b798
mdnurakmal <mdnurakmal@gmail.com>
2021-09-02 00:23:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,9 +13,6 @@ provider "google" {
 
 locals {
 
-  service_folder = "service"
   service_name   = "text-to-speech"
-
-  deployment_name = "cats"
   cats_worker_sa  = "serviceAccount:${google_service_account.text_to_speech_worker.email}"
 }
