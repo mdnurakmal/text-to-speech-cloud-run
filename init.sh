d329f4f5bac94d233530f296796f9d534b65ea9d
mdnurakmal <mdnurakmal@gmail.com>
2021-09-03 01:08:38 +0800

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-export PROJECT_ID=`gcloud projects list --filter="PROJECT" --format="value(PROJECT_ID)"`
-gcloud config set project $PROJECT_ID
 gcloud builds commit
 terraform init
 terraform apply
