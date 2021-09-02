8a226def226ac8ad8a52db62b5abdd299a2da674
mdnurakmal <mdnurakmal@gmail.com>
2021-09-03 01:04:42 +0800

    update

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM gcr.io/cloudshell-images/cloudshell:latest
+
+ENTRYPOINT ["sh","run.sh"]
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export PROJECT_ID=`gcloud projects list --filter="PROJECT" --format="value(PROJECT_ID)"`
+gcloud config set project $PROJECT_ID
+gcloud builds commit
 terraform init
 terraform apply
