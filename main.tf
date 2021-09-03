a7f47d54cda34d482ca5aff63b0452e63366482f
mdnurakmal <mdnurakmal@gmail.com>
2021-09-03 23:59:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,19 +13,8 @@ data "google_project" "project" {
 }
 
 
-
-
 locals {
-
   project = data.google_project.project.project_id
   service_name   = "text-to-speech"
   cats_worker_sa  = "serviceAccount:${google_service_account.text_to_speech_worker.email}"
-}
-
-resource "null_resource" "health_check" {
-
- provisioner "local-exec" {
-
-    command = "echo ${data.google_project.project.region}"
-  }
 }
