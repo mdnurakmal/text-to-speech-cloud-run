bbf6d50483848a2d8b9541079987ca9d6632e40c
mdnurakmal <mdnurakmal@gmail.com>
2021-09-03 22:35:33 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pack build "gcr.io/${$PROJECT_ID}/text-to-speech-cloud-run" --builder gcr.io/buildpacks/builder:v1 --env GOOGLE_ENTRYPOINT="exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app"
+pack build text-to-speech-cloud-run --builder gcr.io/buildpacks/builder:v1 --env GOOGLE_ENTRYPOINT="exec gunicorn --bind :$PORT --workers 1 --threads 8 app:app"
 #terraform init
 #terraform apply
