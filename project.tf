835766d6a79f7190abd72ca15415f11021546d30
mdnurakmal <mdnurakmal@gmail.com>
2021-09-04 10:43:55 +0800

    Update project.tf

--- project.tf
+++ project.tf
@@ -1,23 +1,23 @@
 # Enable services
 resource "google_project_service" "texttospeech" {
   service = "texttospeech.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 resource "google_project_service" "cloudbuild" {
   service = "cloudbuild.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 
