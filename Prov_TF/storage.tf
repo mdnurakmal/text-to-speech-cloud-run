252911993f503ff4c078f61c0241d0457b174aca
mdnurakmal <mdnurakmal@gmail.com>
2021-09-08 22:05:21 +0800

    update

--- Prov_TF/storage.tf
+++ Prov_TF/storage.tf
@@ -2,6 +2,10 @@ resource "google_storage_bucket" "tf_state" {
   name          = "text-to-speech-cloud-run-bucket"
   force_destroy = true
   uniform_bucket_level_access = true
+
+    versioning {
+    enabled = var.versioning_enabled
+  }
 }
 
 terraform {
