8d6f681eb4ae078dd4175b7a63e74bbb5cd9e21e
mdnurakmal <mdnurakmal@gmail.com>
2021-09-11 00:41:09 +0800

    Update build.tf

--- Init_TF/build.tf
+++ Init_TF/build.tf
@@ -3,6 +3,7 @@ resource "null_resource" "health_check" {
  provisioner "local-exec" {
 
     command = <<EOT
+      "echo $PWD"
       "cd $HOME/text-to-speech-cloud-run && gcloud builds submit -q"
       "docker pull gcr.io/${data.google_project.project.project_id}/text-to-speech-cloud-run"
     EOT
