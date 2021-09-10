ebb5671a898717325ffd5cb69fc154e11cc5c802
mdnurakmal <mdnurakmal@gmail.com>
2021-09-10 21:58:45 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,14 @@
 #!bin/bash
-cd Prov_TF && terraform destroy -auto-approve -input=false
+cd Prov_TF
+terraform init
+terraform state pull
+terraform destroy -auto-approve -input=false
+
 cd ..
-cd Init_TF && terraform destroy -auto-approve -input=false
-cd ..
+
+cd Init_TF 
+terraform init
+terraform state pull
+terraform destroy -auto-approve -input=false
+
 gsutil rm -r gs://text-to-speech-cloud-run-bucket
