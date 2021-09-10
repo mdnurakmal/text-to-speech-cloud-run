59b1fdd0bafd397037d685616e06d34e7445fddc
mdnurakmal <mdnurakmal@gmail.com>
2021-09-10 23:13:24 +0800

    Update create_bucket.sh

--- create_bucket.sh
+++ create_bucket.sh
@@ -4,7 +4,8 @@ gsutil -q stat gs://text-to-speech-cloud-run-bucket/terraform
 
 return_value=$?
 
-if [ $return_value = 0 ]; then
+if [[ $return_value = 0 ]] 
+then
     echo "bucket exist"
 else
     gsutil mb gs://text-to-speech-cloud-run-bucket
